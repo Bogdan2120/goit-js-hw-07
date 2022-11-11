51d95060ed92418361655da19b6dfd24059084db
Bohdan Vasylyk <bogdanvasilik2120@gmail.com>
2022-11-11 13:51:42 +0200

    hw-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,60 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+//getItemTemplate
+const getItemTemplate = ({
+  preview,
+  original,
+  description,
+}) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+
+//ModalImg
+const createModalImg = (url) => {
+  const instance = basicLightbox.create(`<img src="${url}" height="auto"/>`, {
+    onShow: (instance) => {
+      instance.element().querySelector("img").onclick = instance.close;
+      document.onkeyup = (e) => {
+        if (e.code !== "Escape") {
+          return;
+        }
+        instance.close();
+      };
+    },
+  });
+
+  instance.show();
+};
+
+function render() {
+  const item = galleryItems.map(getItemTemplate);
+
+  refs.gallery.insertAdjacentHTML("beforeend", item.join(""));
+}
+
+const onImgClick = (e) => {
+  if (e.target === e.currentTarget) {
+    return;
+  }
+  e.preventDefault();
+
+  const urlModalImg = e.target.dataset.source;
+
+  createModalImg(urlModalImg);
+};
+
+render();
+
+refs.gallery.addEventListener("click", onImgClick);
